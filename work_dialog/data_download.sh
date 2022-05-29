d2ef249b91eec63b5aaed18b236e81563df9db83
NatsunoTsukuda <tktkbohshi@gamil.com>
2022-05-29 20:09:10 +0900

    フォルダ構成変更

--- work_dialog/data_download.sh
+++ work_dialog/data_download.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 git clone https://github.com/nttcslab/japanese-dialog-transformers.git
-cp japanese-dialog-transformers/data/dicts/* data/sp
-yes | rm -R japanese-dialog-transformers
 
 wget -P data/pretrained https://www.dropbox.com/s/k3ugxmr7nw6t86l/japanese-dialog-transformer-1.6B.pt
 wget -P data/perchat https://www.dropbox.com/s/sda9wzexh7ntlij/japanese_persona_chat.xlsx

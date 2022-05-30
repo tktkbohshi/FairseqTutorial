e00e2f5677e57af7a8cad87fb239d4214c79a6d9
NatsunoTsukuda <tktkbohshi@gamil.com>
2022-05-30 16:12:11 +0900

    add preprocesiing data for captioning

---
+++ work_caption/download_data.sh
@@ -0,0 +1,9 @@
+# install kaggle API
+pip install kaggle
+
+# download flower dataset
+kaggle competitions download -c datalab-cup3-reverse-image-caption-2021
+
+mkdir data/datalab-cup3-reverse-image-caption-2021
+unzip datalab-cup3-reverse-image-caption-2021.zip data/datalab-cup3-reverse-image-caption-2021
+rm datalab-cup3-reverse-image-caption-2021.zip

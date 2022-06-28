9b76b40276097677d153ba1e0555b230a40090ae
SitaroShinagawa <hirameki88888@gmail.com>
2022-06-28 21:09:59 +0900

    update preprocess pipeline

--- work_dialog/prerpocess.sh
+++ work_dialog/prerpocess.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+
+set -eu
+
+# extrct data into raw texts
+python scripts/extract_persona_chat.py japanese_persona_chat.xlsx data/perchat/raw/
+
+# divide texts into word segments
+python scripts/tokenize_sp.py data/perchat/raw data/perchat/spaced
+
+# run fairseq preprocess
 fairseq-preprocess \
---trainpref data/perchat/train --validpref data/perchat/valid --testpref data/perchat/test \
---source-lang src --target-lang dst \
---destdir data/perchat/bin \
---tokenizer space \
---srcdict data/sp/sp_oall_32k.txt \
---tgtdict data/sp/sp_oall_32k.txt
+    --trainpref data/perchat/spaced/train \
+    --validpref data/perchat/spaced/valid \
+    --testpref data/perchat/spaced/test \
+    --source-lang src \
+    --target-lang dst \
+    --destdir data/perchat/bin \
+    --tokenizer space \
+    --srcdict data/sp/sp_oall_32k.txt \
+    --tgtdict data/sp/sp_oall_32k.txt
+
--- work_dialog/scripts/tokenize_sp.py
+++ work_dialog/scripts/tokenize_sp.py
@@ -1,4 +1,8 @@
 import sentencepiece as spm
+import sys
+
+perchat_src = sys.argv[1] #'data/perchat/raw'
+perchat_dst = sys.argv[2] #'data/perchat/spaced'
 
 sp = spm.SentencePieceProcessor()
 sp.Load("data/sp/sp_oall_32k.model")
@@ -9,18 +13,17 @@ def tokenize(raw_text):
 
 prefs = ['train', 'valid', 'test']
 expands = ['src', 'dst']
-perchat_dir = 'data/perchat'
 
 for p in prefs:
     for e in expands:
         lines = []
-        with open(f'{perchat_dir}/{p}.{e}', mode='r') as f:
+        with open(f'{perchat_src}/{p}.{e}', mode='r') as f:
             lines = f.readlines()
             lines = [ line.strip('\n') for line in lines ]
 
             # Tokenize each text
             lines = [ tokenize(line) for line in lines ]
         
-        with open(f'{perchat_dir}/{p}.{e}', mode='w') as f:
+        with open(f'{perchat_dst}/{p}.{e}', mode='w') as f:
             f.write('\n'.join(lines))
         
